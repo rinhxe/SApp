3bd6ad5b01d443a914dbf1b46df49156c42398d7
haivvph24197 <118285969+haivvph24197@users.noreply.github.com>
2023-05-28 12:41:11 +0700

    SxFav

--- ShoeApp/compoment/Favourite.js
+++ ShoeApp/compoment/Favourite.js
@@ -1,14 +1,14 @@
 import { getAuth } from 'firebase/auth';
 import React, { useEffect, useState } from 'react'
-import { View, StyleSheet, Text, ScrollView, Image } from "react-native";
+import { View, StyleSheet, Text, ScrollView, Image, TouchableHighlight, Modal } from "react-native";
 import firebase from '../config/FirebaseConfig';
 import { getDatabase, off, onValue, ref } from 'firebase/database';
-import { width } from 'deprecated-react-native-prop-types/DeprecatedImagePropType';
 
 export default function Favourite({ props }) {
   const [favProducts, setFavProducts] = useState([]);
-  const auth = getAuth(firebase)
-  const data = getDatabase(firebase)
+  const [showDialog, setshowDialog] = useState(false);
+  const auth = getAuth(firebase);
+  const data = getDatabase(firebase);
 
   useEffect(() => {
     upData();
@@ -37,55 +37,222 @@ export default function Favourite({ props }) {
   const Count = () => {
     return favProducts.length
   }
+  const TC = () => {
+    const sortedProducts = [...favProducts];
+    const n = sortedProducts.length;
+
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (sortedProducts[j].price > sortedProducts[j + 1].price) {
+          // Hoán đổi vị trí
+          const temp = sortedProducts[j];
+          sortedProducts[j] = sortedProducts[j + 1];
+          sortedProducts[j + 1] = temp;
+        }
+      }
+    }
+    setshowDialog(false)
+    setFavProducts(sortedProducts);
+  }
+  const CT = () => {
+    const sortedProducts = [...favProducts];
+    const n = sortedProducts.length;
+
+    for (let i = 0; i < n - 1; i++) {
+      for (let j = 0; j < n - i - 1; j++) {
+        if (sortedProducts[j].price < sortedProducts[j + 1].price) {
+          // Hoán đổi vị trí
+          const temp = sortedProducts[j];
+          sortedProducts[j] = sortedProducts[j + 1];
+          sortedProducts[j + 1] = temp;
+        }
+      }
+    }
+    setshowDialog(false)
+    setFavProducts(sortedProducts);
+  }
+  const closeModal = () => {
+    setshowDialog(false);
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title} >DANH SÁCH YÊU THÍCH </Text>
-      <View style={{ width: '100%', backgroundColor: 'black', height: 1 }} />
-      <Text style={{ marginTop: 15, marginLeft: 15 }}>{Count()} MẶT HÀNG</Text>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={showDialog}
+      >
+        <View style={styles.modalContainer}>
+          <TouchableHighlight
+            activeOpacity={1}
+            underlayColor="rgba(0, 0, 0, 0.5)"
+            onPress={closeModal}
+          >
+            <View />
+          </TouchableHighlight>
+
+          <View style={styles.modalContent}>
+            <View style={styles.modalHeader}>
+              <Text style={styles.modalHeaderText}>SẮP XẾP THEO</Text>
+              <TouchableHighlight
+                activeOpacity={0.6}
+                underlayColor="transparent"
+                style={styles.modalCloseButton}
+                onPress={closeModal}
+              >
+                <Image style={styles.modalCloseIcon} source={require('../image/close.png')} />
+              </TouchableHighlight>
+            </View>
+
+            <View style={styles.modalDivider} />
+
+            <TouchableHighlight
+              activeOpacity={0.6}
+              underlayColor="white"
+              style={styles.modalOption}
+              onPress={TC}
+            >
+
+              <View style={{ flexDirection: 'row' }}>
+                <Image source={require('../image/tc.png')} />
+                <Text style={styles.modalOptionText}>Giá [Thấp - Cao]</Text>
+
+              </View>
+            </TouchableHighlight>
+
+            <TouchableHighlight
+              activeOpacity={0.6}
+              underlayColor="white"
+              style={styles.modalOption}
+              onPress={CT}
+            >
+              <View style={{ flexDirection: 'row' }}>
+                <Image source={require('../image/ct.png')} />
+                <Text style={styles.modalOptionText}>Giá [Cao - Thấp]</Text>
+
+              </View>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+
+      <Text style={styles.title}>DANH SÁCH YÊU THÍCH </Text>
+      <View style={styles.titleDivider} />
+      <View style={styles.infoContainer}>
+        <Text>{Count()} MẶT HÀNG</Text>
+        <TouchableHighlight
+          activeOpacity={0.6}
+          underlayColor="white"
+          onPress={() => { setshowDialog(true) }}
+        >
+          <Image source={require('../image/sx.png')} />
+        </TouchableHighlight>
+      </View>
 
       <ScrollView style={styles.frame}>
         {favProducts.map((product) => (
-
           <View key={product.id} style={styles.productContainer}>
             <View style={styles.productBox}>
               <Image source={{ uri: product.search_image }} style={styles.productImage} />
-
               <View style={styles.productInfo}>
                 <Text style={styles.productName}>{product.brands_filter_facet}</Text>
                 <Text style={styles.productPrice}>Giá: {product.price} VNĐ</Text>
-                <View style={{
-                  flexDirection: 'row', padding: 3, borderWidth: 1,
-                  borderColor: '#000000',width:115,backgroundColor:'white'
-                }}>
-                  <Text style={{fontSize:13}}>Thêm vào giỏ</Text>
-                  <Image style={{ width: 20, height: 20, marginLeft: 5 }} source={require('../image/addcar.png')} />
+                <View style={styles.addButton}>
+                  <Text style={styles.addButtonLabel}>Thêm vào giỏ</Text>
+                  <Image style={styles.addButtonIcon} source={require('../image/addcar.png')} />
                 </View>
               </View>
               <Image source={require('../image/More.png')} />
             </View>
-
-
-
           </View>
         ))}
       </ScrollView>
     </View>
   )
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  productImage: {
-    width: 90,
-    height: 60,
-    resizeMode: 'cover',
-    borderRadius: 8,
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'flex-end',
   },
-  productPrice:{
-    fontWeight:'bold',
-    marginTop:3,
-    marginBottom:3
+
+  modalContent: {
+    width: '100%',
+    backgroundColor: 'white',
+    justifyContent: 'flex-end',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  modalHeader: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+
+  },
+  modalHeaderText: {
+
+    fontSize: 23,
+    fontWeight: 'bold',
+    margin: 14,
+  },
+  modalCloseButton: {
+    marginTop: 15,
+    marginLeft: 'auto',
+  },
+  modalCloseIcon: {
+    width: 40,
+    height: 40,
+  },
+  modalDivider: {
+    width: '100%',
+    borderBottomWidth: 1,
+  },
+  modalOption: {
+    borderColor: '#5F5F5F',
+    borderWidth: 1,
+    width: '100%',
+    padding: 20,
+    paddingLeft: 23,
+  },
+  modalOptionText: {
+    fontSize: 19,
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginTop: 40,
+    marginLeft: 15,
+    marginBottom: 7,
+  },
+  titleDivider: {
+    width: '100%',
+    backgroundColor: 'black',
+    height: 1,
+  },
+  infoContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginTop: 15,
+    marginLeft: 15,
+    marginRight: 15,
+  },
+  frame: {
+    marginRight: 14,
+    marginLeft: 14,
+  },
+  productContainer: {
+    marginTop: 14,
   },
   productBox: {
     alignItems: 'center',
@@ -93,24 +260,39 @@ const styles = StyleSheet.create({
     padding: 10,
     backgroundColor: '#D7D8D9',
     flexDirection: 'row',
-    marginTop: 14
-
   },
-  frame: {
-    marginRight: 14,
-
-    marginLeft: 14,
+  productImage: {
+    width: 90,
+    height: 60,
+    resizeMode: 'cover',
+    borderRadius: 8,
   },
   productInfo: {
     marginLeft: 14,
-    width: 220
+    width: 220,
   },
-  title: {
-    fontSize: 18,
+  productName: {
     fontWeight: 'bold',
-    marginTop: 40,
-    marginLeft: 15,
-    marginBottom: 7
-
   },
-})
+  productPrice: {
+    fontWeight: 'bold',
+    marginTop: 3,
+    marginBottom: 3,
+  },
+  addButton: {
+    flexDirection: 'row',
+    padding: 3,
+    borderWidth: 1,
+    borderColor: '#000000',
+    width: 115,
+    backgroundColor: 'white',
+  },
+  addButtonLabel: {
+    fontSize: 13,
+  },
+  addButtonIcon: {
+    width: 20,
+    height: 20,
+    marginLeft: 5,
+  },
+});
