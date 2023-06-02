00532a7609adef09e0c403abddf69a872bae946f
KienPC-KEN <phungchikien196@gmail.com>
2023-06-02 12:47:40 +0700

    /

--- Server_ShoeApp/server.js
+++ Server_ShoeApp/server.js
@@ -68,7 +68,7 @@ app.delete('/delete/:id', async (req, res) => {
     }
 })
 
-const PORT = process.env.PORT || 9999;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
     console.log('Server is running');
